b4f691724dccf88e2e25175e7ee3ea45f50f3906
Nishant <nishant2024rde@gmail.com>
2024-11-07 09:36:08 -0800

    Initial Commit

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -3,11 +3,13 @@
 from rest_framework import serializers
 from .models import User, Chat
 
+# For return User feilds 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'tokens']
 
+# For returning Chat feilds
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
--- mainapp/views.py
+++ mainapp/views.py
@@ -15,20 +15,22 @@ from rest_framework.authentication import TokenAuthentication
 from django.db import transaction, IntegrityError
 from django.db import IntegrityError, transaction
 
-
+# Registration 
 class RegisterView(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
-        
+
+        # see if user exists and if not returning status HTTP_400_BAD_REQUEST
         if User.objects.filter(username=username).exists():
             return Response({"error": "Username already exists."}, status=status.HTTP_400_BAD_REQUEST)
-
+        
+        # creating user returning status HTTP_201_CREATED
         user = User.objects.create(username=username, password=make_password(password), tokens=4000)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
+# Login
 class LoginView(APIView):
     def post(self, request):
         username = request.data.get("username")
@@ -57,14 +59,16 @@ class LoginView(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+# Chat_API
 class ChatView(APIView):
+    # Token authentication
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
         user = request.user
         message = request.data.get("message")
-
+        # See weather the user have sufficient toke and if not returning status HTTP_403_FORBIDDEN
         if user.tokens < 100:
             return Response({"error": "Insufficient tokens."}, status=status.HTTP_403_FORBIDDEN)
 
@@ -84,11 +88,14 @@ class ChatView(APIView):
             "remaining_tokens": user.tokens
         }, status=status.HTTP_200_OK)
 
+# Balance 
 class TokenBalanceView(APIView):
+    # Token authentication
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        # retuning the tokens left for the user and status HTTP_200_OK
         user = request.user
         return Response({
             "user" : user.username,
